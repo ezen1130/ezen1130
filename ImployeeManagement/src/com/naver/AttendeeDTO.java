94d4516b3b82592d72c41556f95ad7053c5d3300
YSS513 <72856989+YSS513@users.noreply.github.com>
2020-11-30 10:59:07 +0900

    출근자조회, 사원삭제 2가지 기능입니다.

--- ImployeeManagement/src/com/naver/AttendeeDTO.java
+++ ImployeeManagement/src/com/naver/AttendeeDTO.java
@@ -3,51 +3,64 @@ package com.naver;
 import java.io.Serializable;
 import java.sql.Date;
 
-public class AttendeeDTO implements Serializable{
-
+public class AttendeeDTO implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String id;
 	private String name;
-	private String intime;
-	private String exittime;
-	
-	public String getId() {
-		return id;
+	private Date intime;
+	private Date exittime;
+
+	public AttendeeDTO() {
+		// TODO Auto-generated constructor stub
 	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getIntime() {
-		return intime;
-	}
-	public void setIntime(String intime) {
-		this.intime = intime;
-	}
-	public String getExittime() {
-		return exittime;
-	}
-	public void setExittime(String exittime) {
-		this.exittime = exittime;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	public AttendeeDTO(String id, String name, String intime, String exittime) {
+
+	public AttendeeDTO(String id, String name, Date intime, Date exittme) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.intime = intime;
-		this.exittime = exittime;
+		this.exittime = exittme;
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getIntime() {
+		return intime;
+	}
+
+	public void setIntime(Date intime) {
+		this.intime = intime;
+	}
+
+	public Date getExittme() {
+		return exittime;
+	}
+
+	public void setExittme(Date exittme) {
+		this.exittime = exittme;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -55,6 +68,7 @@ public class AttendeeDTO implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -71,10 +85,12 @@ public class AttendeeDTO implements Serializable{
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "AttendeeDTO [id=" + id + ", name=" + name + ", intime=" + intime + ", exittime=" + exittime + "]";
 	}
-
-
+	
+	
+	
 }
